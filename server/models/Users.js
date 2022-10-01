2d83e5a90c749cc7684b4e71b4c3036a7632ea63
aleja <92136921+mariather@users.noreply.github.com>
2022-09-30 21:13:17 -0700

    add schema specifications on user model

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,42 +4,59 @@ const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      lastName: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true
-      },
-      password: {
-        type: String,
-        required: true,
-        minlength: 5
-      }
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 30,
+    minlength: 3
+
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 30,
+    minlength: 2
+  },
+  email: {
+    type: String,
+    required: 'Email is required',
+    unique: true,
+    match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: 'Password is required',
+    minlength: 6,
+    maxlength: 30
+  },
+  profile_picture: {
+    type: String,
+    default: null
+  },
+  servers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Server',
+    },
+  ],
 });
 
-userSchema.pre('save', async function(next) {
-    if (this.isNew || this.isModified('password')) {
-      const saltRounds = 10;
-      this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-  
-    next();
-  });
 
-userSchema.methods.isCorrectPassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
-  };
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
- 
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
