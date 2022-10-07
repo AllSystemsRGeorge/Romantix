edfca7a2d58a3556789cdaf2cf0e5b8a24d6bf68
aleja <92136921+mariather@users.noreply.github.com>
2022-10-06 20:34:20 -0700

    create posters and url code for each row

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,8 +1,12 @@
-import React, { useInsertionEffect, useState } from 'react'
+import React from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './row.css'
 
-function Row({ title }) {
-  //Array of movies
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl }) {
+    //Array of movies
   const [movies, setMovies] = useState([]);
 
   // this is a piece of the code that will run based on specific condition/variable. It will pull the information from
@@ -11,12 +15,32 @@ function Row({ title }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
-    
+
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+  
+  console.log(movies);
+
+    return(
+        <div className='row'>
+          <h2>{ title }</h2>
+
+          <div className='row_posters'>
+            {/* several row_poster(s) container*/}
+            {movies.map(movie => (
+                <img
+                className='row_poster'
+                src={`${base_url}${movie.poster_path}`}alt={movie.name}/>
+            ))}
+          </div>
+          
+
+        </div>
+    )
+            
 
   return (
     <div>
